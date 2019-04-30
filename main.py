e77cdb200713dc3ebb2394675488bc8e8c6d40f9
Angel Lopez Manriquez <sigma271@outlook.com>
2019-04-30 15:01:45 -0500

    3 grids de 4

--- main.py
+++ main.py
@@ -1,10 +1,35 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-@app.route('/')
-def index():
-    return render_template('index.html')
+
+from flaskext.mysql import MySQL 
+
+# https://stackoverflow.com/questions/9845102/using-mysql-in-flaromk
+mysql = MySQL()
+app.config['MYSQL_DATABASE_USER'] = 'root'
+app.config['MYSQL_DATABASE_PASSWORD'] = 'root'
+app.config['MYSQL_DATABASE_DB'] = 'see'
+app.config['MYSQL_DATABASE_HOST'] = 'localhost'
+mysql.init_app(app)
+
+conn = mysql.connect()
+cursor = conn.cursor()
+
+
+@app.route('/', methods=['GET', 'POST'])
+def horizontal():
+    cursor.execute("SHOW TABLES")
+    relations = cursor.fetchall() # tuple of tuples
+
+    relation_sel = request.form.get('sel-relation')
+    if relation_sel != None:
+        cursor.execute("DESC {}".format(relation_sel))
+        relation_attr = cursor.fetchall()
+        return render_template('horizontal.html', relations=relations, 
+            relation_attr=relation_attr)
+    return render_template('horizontal.html', relations=relations, 
+        relation_attr=False)
 
 if __name__ == "__main__":
     app.run(debug=True)
