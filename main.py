cf5b748b33727ebbfcbfac58bd16cc023db73349
Angel Lopez Manriquez <sigma271@outlook.com>
2019-05-02 23:41:04 -0500

    Persistencia agregada.

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, session
 
 app = Flask(__name__)
 
@@ -17,26 +17,44 @@ mysql.init_app(app)
 conn = mysql.connect()
 cursor = conn.cursor()
 
+# global variables
+cursor.execute('SHOW TABLES')
+relations = cursor.fetchall()  # relations = cursor.fetchall() # tuple of tuples
+
+predicates = set() # of (tuple(str, str, str))
+selected_relation = relations[0][0]
+
+relation_attr = []
+minterm_predicates = []
+
+def __selected_relation():
+    pass
+
+negate_predicate = lambda predicate: '(NOT %s)' % predicate
+predicate_as_str = lambda p: ' '.join(( p[0], p[1], p[2] ))
+
 @app.route('/', methods=['GET', 'POST'])
 def horizontal():
-    cursor.execute("SHOW TABLES")
-    relations = cursor.fetchall() # tuple of tuples
-
-    relation_attr = False
-    predicate = False
+    global predicates, relation_attr, selected_relation, minterm_predicates
 
     if request.method == 'POST':
-        # if 'id-load-relation' in request.form:
-        relation_sel = request.form.get('sel-relation', relations[0][0]) # second argument as default, from the first relation, get the name
-        cursor.execute("DESC {}".format(relation_sel))
-        relation_attr = cursor.fetchall()
+        if 'id-load-relation' in request.form:
+            selected_relation = request.form.get('sel-relation', relations[0][0]) # second argument as default, from the first relation, get the name
+            cursor.execute( "DESC {}".format(selected_relation) )
+            relation_attr = cursor.fetchall()
         if 'id-add-predicate' in request.form: # determines which form was submitted
             attribute = request.form.get('sel-attribute')           
             operator = request.form.get('sel-operator')
             value = request.form.get('txt-value')
-            predicate = ' '.join((attribute, operator, value)) # second argument must be an iterable object
-    return render_template('horizontal.html', relations=relations, relation_attr=relation_attr, 
-            predicate=predicate)
+            predicates.add( (attribute, operator, value) )
+        if "id-build-minterms" in request.form:
+            minterm_predicates = [ ]
+            for p in predicates:
+                minterm_predicates.extend(( predicate_as_str(p), negate_predicate(predicate_as_str(p)) ))
+
+    return render_template( 'horizontal.html', relations=relations, relation_attr=relation_attr, 
+            selected_relation=selected_relation, minterm_predicates=minterm_predicates,
+            predicates=tuple( ( i, predicate_as_str(p) ) for i, p in tuple(enumerate(predicates)) ) )
 
 if __name__ == "__main__":
     app.run(debug=True)
