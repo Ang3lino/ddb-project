801fcc5cca45009f9364df1d3f39c127d645d67b
Angel Lopez Manriquez <sigma271@outlook.com>
2019-05-04 16:00:00 -0500

    Clase puesta en un .py

--- forms/SelectRelationForm.py
+++
@@ -1,9 +0,0 @@
-
-from flask_wtf import FlaskForm
-from wtforms import SelectField, StringField, SubmitField
-
-class SelectRelationForm(FlaskForm):
-    def __init__(self, relations):
-        """ relations: list<tuple>
-        """
-        self.relations = SelectField('Relacion', relations) 
