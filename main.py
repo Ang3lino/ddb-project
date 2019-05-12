1249ffc71937c1e55d90b9a173f0c07b114f3d09
Angel Lopez Manriquez <sigma271@outlook.com>
2019-05-11 21:06:28 -0500

    Ya casi esta

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 from flask import Flask, render_template, request, session, jsonify, flash
+from flaskext.mysql import MySQL 
 
 from my_forms import ProjectionForm
 from pyclasses.predicate import Predicate
 
-app = Flask(__name__)
+import itertools
 
-from flaskext.mysql import MySQL 
+app = Flask(__name__)
 
 app.config['SECRET_KEY'] = 'mysecretkey' # this also enables session
 
@@ -55,18 +56,18 @@ def count_rows(predicate, relation_name):
 def fetch_result_set(cursor, relation_name, predicate=False):
     query = f"SELECT * FROM {relation_name} " + (
         f" WHERE {predicate}" if predicate else "" )
-    print(query)
+    # print(query)
     cursor.execute(query)
     return cursor.fetchall()
 
-def is_complete(predicates, relation_name): # complexity n ** 2 lg n
+def is_complete(predicates, relation_name): 
     resultset = fetch_result_set(cursor, relation_name)
     universe = { t: 0 for t in resultset }
     for predicate in predicates:
         resultset = fetch_result_set(cursor, relation_name, str(predicate))
         for t in resultset: universe[t] += 1
-    for k in universe.keys(): print(k, ' => ', universe[k])
-    m = next(iter(universe.values())) # fetch a random value from the dict
+    # for k in universe.keys(): print(k, ' => ', universe[k])
+    m = next(iter(universe.values())) # fetch the first value from the dict
     return 0 < m and all(m == value for value in universe.values()) # all have the same probability to be accessed
 
 def is_minimal(predicate, relation_name):
@@ -91,14 +92,43 @@ def vertical():
     return render_template('vertical.html', proj_form=projection_form)
 
 def horizontal_handle_add_predicate(request):
-    attribute = request.form.get('sel-attribute')           
-    operator = request.form.get('sel-operator')
-    value = request.form.get('txt-value')
+    form = request.form 
+    attribute = form.get('sel-attribute')           
+    operator = form.get('sel-operator')
+    value = form.get('txt-value')
     predicate =  Predicate(attribute, operator, value) 
     if is_minimal(predicate, selected_relation):
         predicates.append(predicate)
     else: 
         flash("La seleccion sobre el predicado da conjunto vacio, no fue agregado.")
+
+def all_combinations(ss):
+    return itertools.chain(*map(lambda x: itertools.combinations(ss, x), 
+            range(0, len(ss) + 1)))
+
+def remove_repeated_negations(predicates):
+    repeated = list()
+    for i, _ in enumerate(predicates):
+        for j in range(i + 1, len(predicates)):
+            if predicates[i].is_negated(predicates[j]):
+                repeated.append(predicates[j])
+    return list( filter(lambda e: not (e in repeated), predicates) )
+
+def remove_all(source, targets):
+    return list( filter(lambda e: not (e in targets), source) )
+
+def horizontal_controller_handle_build_terms(predicates, selected_relation):
+    if is_complete(predicates, selected_relation): flash("El conjunto de predicados es completo.") 
+    predicates = remove_repeated_negations(predicates)
+    minterms = []
+    for combination in filter(lambda e: len(e) > 0, all_combinations(predicates)): # remove the empty chain for all combination 
+        minterms.extend(Predicate.minterms( list(combination) ))
+    minterms = [ m for m in minterms if is_minimal(m, selected_relation) ] # remove non relevant minterms 
+    combinations = itertools.combinations(minterms, 3) # returns an iterable object, combinations
+    for combination in combinations:
+        if is_complete(combination, selected_relation):
+            print('DONE', combination)
+    # print(minterms)    
 
 @app.route('/', methods=['GET', 'POST'])
 def horizontal():
@@ -112,9 +142,11 @@ def horizontal():
         if 'id-add-predicate' in request.form: # determines which form was submitted
             horizontal_handle_add_predicate(request)
         if "id-build-minterms" in request.form:
-            if is_complete(predicates, selected_relation): flash("El conjunto de predicados es completo.") 
-            minterm_predicates = Predicate.minterms(predicates)
-
+            # horizontal_controller_handle_build_terms(predicates, selected_relation)
+            horizontal_controller_handle_build_terms(
+                    [ Predicate('numero', '<', '3'), Predicate('numero', '<', '6'), 
+                            Predicate('numero', '>=', '3'), Predicate('numero', '>=', '6') ], 
+                    'sala_votacion')
     return render_template( 'horizontal.html', relations=relations, relation_attr=relation_attr, 
             selected_relation=selected_relation, minterm_predicates=minterm_predicates,
             predicates=tuple( ( i, str(p) ) for i, p in tuple(enumerate(predicates)) ) )
