3327516acd5fa66b84b8756404f130efcace292a
Parrot Live user <user@localhost.localdomain>
2019-05-23 14:24:43 +0000

    jic

--- config.py
+++ config.py
@@ -5,7 +5,7 @@ SECRET_KEY = 'mysecretkey' # this also enables session
 
 # Mysql config
 MYSQL_DATABASE_USER = 'root'
-MYSQL_DATABASE_PASSWORD = ''
+MYSQL_DATABASE_PASSWORD = 'root'
 MYSQL_DATABASE_DB = 'see'
 MYSQL_DATABASE_HOST = 'localhost'
 
--- db.py
+++ db.py
@@ -4,6 +4,19 @@ class DbHelper:
         self.cursor = cursor 
         self.connection = connection
 
+    def create_vertical_fragment(self, dbsrc, dbtarget, tablename, attributes):
+        '''
+        All argument is str type is expected with the exception of attributes which are of type
+        iter(iter(d)) where d = (Field, Type, Null, Key)
+        '''
+        self.cursor.execute(f'DROP DATABASE IF EXISTS {dbtarget}') 
+        self.cursor.execute(f'CREATE DATABASE {dbtarget}') 
+        metadata = ', '.join(map(lambda d: str(d[0]) + ' ' + str(d[1]), attributes)) 
+        self.cursor.execute(f'CREATE TABLE {tablename} ({metadata})') 
+        selectquery = f"SELECT {', '.join(map(labda d: str(d[0])))} FROM {tablename}"
+        self.cursor.execute(f'INSERT INTO {dbtarget}.{tablename} {selectquery}') 
+        self.connection.commit() # call commit after inserting
+
     def relation_attributes(self, relation_name):
         """
         Arguments:
